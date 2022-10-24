9379c81a25d7a984f987418e8a44211cc4768237
Callmeoli <olikorma@gmail.com>
2022-10-24 20:26:28 +0300

    First_commit_files_models/base_model.py,_models/__init__.py

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import copy
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ Base model """
+
+    def __init__(self):
+        """public instance"""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return str representations"""
+        return "[{}] ({}) {}".format(self.__class__, self.id, self.__dict__)
+
+    def save(self):
+        """ Edit the updated time """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Returns a dictionary containing all keys/values of the instance"""
+        dic = copy.deepcopy(self.__dict__)
+        dic['class'] = self.__class__.__name__
+        dic['created_at'] = dic['created_at'].isoformat()
+        dic['updated_at'] = dic['updated_at'].isoformat()
+        return dic
