dfea3d16fe361e43a77823936cd325dd3749aa2f
AbdiAbader <abdiprocode@gmail.com>
2022-10-25 20:54:55 +0300

    add args and kwargs for converting dic to class instance

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,21 @@ from datetime import datetime
 
 class BaseModel():
     """ Basemodel class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.updated_at = datetime.now()
-        self.created_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            var = vars()
+            for k ,v in kwargs.items():
+                if k == "created_at":
+                    self.created_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "updated_at":
+                    self.updated_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+
     def __str__(self):
         """string rep of  class object"""
         return f'[{__class__.__name__}] ({self.id}) ({self.__dict__})'
@@ -20,17 +31,8 @@ class BaseModel():
         self.updated_at = datetime.now()
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
-        """d = self.__dict__.copy()
+        d = self.__dict__.copy()
         d["created_at"] = self.created_at.isoformat()
         d["updated_at"] = self.updated_at.isoformat()
         d["__class__"] = self.__class__.__name__
-        return d
-        """
-        d = {}
-        d['__class__'] = __class__.__name__
-        for k ,v in self.__dict__.items():
-            if "created_at" == k or "updated_at" == k:
-                d[k] = v.isoformat()
-                continue
-            d[k] = v
         return d
