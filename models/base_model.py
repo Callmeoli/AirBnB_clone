8b968efa1227420373d29fd5ccc2a12fe1bf5ca5
Callmeoli <olikorma@gmail.com>
2022-10-25 11:38:35 +0300

    added *args to init method BaseModel Added to init method to create base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,27 @@ from datetime import datetime
 class BaseModel:
     """ Base model """
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """public instance"""
-        if kwargs != 0:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
-                setattr(self, key, value)
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                if key == "__class__":
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self,
+                            key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return str representations"""
-        return "[{}] ({}) {}".format (self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ Edit the updated time """
@@ -27,7 +36,7 @@ class BaseModel:
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of the instance"""
         dic = copy.deepcopy(self.__dict__)
-        dic['id'] = str(dic['id'])
+        dic['id'] = dic['id']
         dic['__class__'] = self.__class__.__name__
         dic['created_at'] = dic['created_at'].isoformat()
         dic['updated_at'] = dic['updated_at'].isoformat()
--- test_base_model.py
+++ test_base_model.py
@@ -5,6 +5,7 @@ my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
+print(type(my_model.created_at))
 my_model.save()
 print(my_model)
 my_model_json = my_model.to_dict()
