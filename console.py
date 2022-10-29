08f79b24a4ef711aa8deea9258176566d57ed83d
AbdiAbader <abdiprocode@gmail.com>
2022-10-29 13:49:50 +0300

    fix all and update methods

--- console.py
+++ console.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-""" cmd module """
-
+""""Uses Cmd module"""
+import re
 import cmd
 from models.base_model import BaseModel
-import shlex
-from models import storage
 from models.user import User
-from models.place import Place
-from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
+from models.state import State
 from models.review import Review
-
-
+from models import storage
 class HBNBCommand(cmd.Cmd):
-    """ entry point of the command interpreter: """
     """Class HBNB"""
     prompt = "(hbnb)"
-
     def do_quit(self, line):
         """ Quit command to exit the program """
         return True
@@ -41,116 +36,110 @@ class HBNBCommand(cmd.Cmd):
             ins.save()
             print(ins.id)
 
-    def do_show(self, arg):
-        """  Prints the string representation """
-        if not arg:
+    def do_show(self, line):
+        """show instance"""
+        a = []
+        for m in line.split(" "):
+            a.append(m)
+        if len(line) == 0:
             print("** class name missing **")
-        elif len(shlex.split(arg)) == 1:
-            print("** instance id missing **")
-        elif not ((shlex.split(arg))[0] in self.classes):
+        elif globals().get(a[0]) is None:
             print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        elif a[0]+"."+a[1] not in storage.all():
+            print("** no instance found **")
         else:
+            print(storage.all()[a[0]+"."+a[1]])
 
-            key = "{}.{}" \
-                .format((shlex.split(arg))[0], ((shlex.split(arg))[1]))
-            obj_s = storage.all()
-            if key not in obj_s.keys():
-                print("** no instance found **")
-                return
-            for k, v in obj_s.items():
-                if key == k:
-                    print(v)
-
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id """
-
-        if not arg:
+    def do_destroy(self, line):
+        """ Deletes an instance based on the class name and id """
+        a = []
+        for m in line.split(" "):
+            a.append(m)
+        if len(line) == 0:
             print("** class name missing **")
-        elif len(shlex.split(arg)) == 1:
-            print("** instance id missing **")
-        elif not (shlex.split(arg))[0] in self.classes:
+        elif globals().get(a[0]) is None:
             print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        elif a[0] + "." + a[1] not in storage.all():
+            print("** no instance found **")
         else:
-            key = "{}.{}" \
-                .format((shlex.split(arg))[0], ((shlex.split(arg))[1]))
-            obj = storage.all()
-            if not (key in obj.keys()):
-                print("** no instance found **")
-                return
-            del obj[key]
+            del storage.all()[a[0] + "." + a[1]]
             storage.save()
-
-    def do_all(self, arg):
-        """  Prints all string representation of all instances """
-        to_list = []
-        if len(shlex.split(arg)) == 1 and \
-                (shlex.split(arg))[0] in self.classes:
-            obj_l = storage.all()
-            for k, v in obj_l.items():
-                y = k.split(".")[0]
-                if arg == y:
-                    to_list.append(str(obj_l[k]))
-            print(to_list)
-        elif len(shlex.split(arg)) == 0:
-            obj_l = storage.all()
-            for k, v in obj_l.items():
-                to_list.append(str(obj_l[k]))
-            print(to_list)
-        else:
+    def do_all(self, line):
+        """  Prints all string representation of all instances based or not on the class name """
+        if len(line) == 0:
+            for k, v in storage.all().items():
+                print(storage.all())
+        elif globals().get(line) is None:
             print("** class doesn't exist **")
+        else:
+            for k, v in storage.all().items():
+                if line in k:
+                    print(storage.all()[k])
+    def default(self, line):
+        """custom """
+        a = []
+        for i in line.split('.'):
+            a.append(i)
+        if len(line) < 1:
+            print("format Error")
+        elif globals().get(a[0]) is None:
+            print("** class doesn't exist **")
+        else:
+            check_c = re.sub("[\(\[].*?[\)\]]", "", a[1])
+            #print(a[1][len(re.sub("[\(\[].*?[\)\]]", "", a[1]))+2:-2])
+            if check_c == "all":
+                HBNBCommand.do_all(self, a[0])
+            elif check_c == "count":
+                HBNBCommand.count(self, a[0])
+            elif check_c == "show":
+                HBNBCommand.do_show(self,a[0]+" "+a[1][len(re.sub("[\(\[].*?[\)\]]", "", a[1]))+2:-2])
+            elif check_c == "destory":
+                HBNBCommand.do_destroy(self, a[0] + " " + a[1][len(re.sub("[\(\[].*?[\)\]]", "", a[1])) + 2:-2])
+            elif check_c == "update":
+                lists = a[1][len(re.sub("[\(\[].*?[\)\]]", "", a[1])) + 1:-1].split(",")
+                l1 = lists[0].replace('"',"").strip()
+                p = lists[1].replace('"',"").strip() + " " + lists[2].replace('"',"").strip()
+                HBNBCommand.do_update(self, a[0] + " " + l1 + " " + p)
 
-    def do_update(self, arg):
-        """ Updates an instance based on the class name and id """
-        if len(shlex.split(arg)) == 0:
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id by adding or updating attribute """
+        a = []
+        for m in line.split(" "):
+            a.append(m)
+        if len(line) == 0:
             print("** class name missing **")
-        elif len(shlex.split(arg)) == 1:
-            if not (shlex.split(arg))[0] in self.classes:
-                print("** class doesn't exist **")
-                return
-            else:
-                print("** instance id missing **")
-        elif len(shlex.split(arg)) == 2:
-            if not (shlex.split(arg))[0] in self.classes:
-                print("** class doesn't exist **")
-                return
-            key = "{}.{}" \
-                .format((shlex.split(arg))[0], ((shlex.split(arg))[1]))
-            obj_u = storage.all()
-            if not (key in obj_u.keys()):
-                print("** no instance found **")
-                return
-        elif len(shlex.split(arg)) == 3:
-            if not (shlex.split(arg))[0] in self.classes:
-                print("** class doesn't exist **")
-                return
-            key = "{}.{}" \
-                .format((shlex.split(arg))[0], ((shlex.split(arg))[1]))
-            obj_u = storage.all()
-            if not (key in obj_u.keys()):
-                print("** no instance found **")
-                return
-            if not (shlex.split(arg))[2] in obj_u[key].keys():
-                print("** attribute name missing **")
-                return
+        elif globals().get(a[0]) is None:
+            print("** class doesn't exist **")
+        elif len(a) == 1:
+            print("** instance id missing **")
+        elif a[0] + "." + a[1] not in storage.all():
+            print("** no instance found **")
+        elif len(a) == 2:
+            print("** attribute name missing **")
+        elif len(a) == 3:
             print("** value missing **")
-        elif len(shlex.split(arg)) == 4:
-            if not (shlex.split(arg))[0] in self.classes:
-                print("** class doesn't exist **")
-                return
-            key = "{}.{}" \
-                .format((shlex.split(arg))[0], ((shlex.split(arg))[1]))
-            obj_u = storage.all()
-            if not (key in obj_u.keys()):
-                print("** no instance found **")
-                return
-
-            if not (hasattr(obj_u[key], shlex.split(arg)[2])):
-                print("** attribute name missing **")
-            else:
-                setattr(obj_u[key], (shlex.split(arg))[2],
-                        (shlex.split(arg))[3])
-            storage.save()
-
-
+        else:
+            for k,v in storage.all().items():
+               if a[0] + "." + a[1] == k:
+                   #print(storage.all()[k])
+                   setattr(storage.all()[k], a[2], a[3])
+                   storage.save()
+                   break
+    def count(self, line):
+        """ count instances"""
+        i = 0
+        if len(line) == 0:
+            print("** class name missing **")
+        elif globals().get(line) is None:
+            print("** class doesn't exist **")
+        else:
+            for k, v in storage.all().items():
+                if line in k:
+                    i = i + 1
+            print(i)
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
